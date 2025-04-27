ccb31ed0d152606be01af95f498193a3cdd3bf6a
Anton Serdyuk <60281710+Serdyuk-prog@users.noreply.github.com>
2025-04-27 22:52:48 +0700

    feat: added icon to footer

--- src/app/MainLayout/ui/MainFooter/MainFooter.tsx
+++ src/app/MainLayout/ui/MainFooter/MainFooter.tsx
@@ -1,18 +1,22 @@
 import './MainFooterStyles.css';
+import logo from '@shared/assets/a11y.svg';
 export const MainFooter = () => {
     return (
-        <footer aria-label='footer' className="main-footer">
-            <nav className="main-footer-block main-footer-nav" aria-label='bottom-navigation'>
+        <footer aria-label="footer" className="main-footer">
+            <nav
+                className="main-footer-block main-footer-nav"
+                aria-label="bottom-navigation"
+            >
                 <div>WCAG 2.2</div>
                 <div className="main-footer-links">
-                    <h5>Карта сайта</h5>
+                    <span>Карта сайта</span>
                     <a href="/">Воспринимаемость</a>
                     <a href="/">Управляемость</a>
                     <a href="/">Понятность</a>
                     <a href="/">Надежность</a>
                 </div>
                 <div className="main-footer-links">
-                    <h5>Полезные ссылки</h5>
+                    <span>Полезные ссылки</span>
                     <a href="/">Ссылка 1</a>
                     <a href="/">Ссылка 2</a>
                     <a href="/">Ссылка 3</a>
@@ -21,7 +25,7 @@ export const MainFooter = () => {
             </nav>
             <div className="main-footer-block main-footer-copyright">
                 <p>Made by @tram_tony</p>
-                <p>No cookies Just accessibility</p>
+                <p>No cookies Just accessibility <img src={logo} alt="Accessibility logo" /></p>
                 <p>
                     Copyright © 2025 All Rights Reserved. CC BY-NC-ND License.
                 </p>
