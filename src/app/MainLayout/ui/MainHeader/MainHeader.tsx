461ea28f0c72cb75f4314ac2241c52745dd3aa6f
Anton Serdyuk <60281710+Serdyuk-prog@users.noreply.github.com>
2025-04-28 20:12:26 +0700

    feat: adaptive main layout

--- src/app/MainLayout/ui/MainHeader/MainHeader.tsx
+++ src/app/MainLayout/ui/MainHeader/MainHeader.tsx
@@ -2,16 +2,18 @@ import './MainHeaderStyles.css';
 
 export const MainHeader = () => {
     return (
-        <nav className="main-header" aria-label='navigation'>
-          <div>
-            <a className='main-header-logo' href="/">WCAG 2.2</a>
-          </div>
-          <div className='main-header-links'>
-            <a href="/">Воспринимаемость</a>
-            <a href="/">Управляемость</a>
-            <a href="/">Понятность</a>
-            <a href="/">Надежность</a>
-          </div>
-        </nav>
+        <header className="main-header" aria-label="navigation">
+            <div>
+                <a className="main-header-logo" href="/">
+                    WCAG 2.2
+                </a>
+            </div>
+            <nav className="main-header-links">
+                <a href="/">Воспринимаемость</a>
+                <a href="/">Управляемость</a>
+                {/* <a href="/">Понятность</a> */}
+                {/* <a href="/">Надежность</a> */}
+            </nav>
+        </header>
     );
 };
