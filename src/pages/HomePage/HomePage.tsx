076e0e57b40cc750983d3058e93350a2ee7c8253
Anton Serdyuk <60281710+Serdyuk-prog@users.noreply.github.com>
2025-05-23 22:10:48 +0700

    feat: added content to home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,37 +1,26 @@
-import BlockModal from '@/shared/modals/BlockModal/BlockModal';
-import DialogModal from '@/shared/modals/DialogModal/DialogModal';
-import { useRef, useState } from 'react';
 import './HomePage.css';
 import { PrincipleCard } from '@/entities/PrincipleCard/PrincipleCard';
 import { TextLink } from '@/shared/ui/TextLink/TextLink';
 import { ButtonLink } from '@/shared/ui/ButtonLink/ButtonLink';
 import useWindowSize from '@/shared/customHooks/useWindowSize';
-import { FaBrain, FaBriefcase, FaChartBar, FaHandPaper, FaRegEye } from 'react-icons/fa';
-import { FaGear, FaHand } from 'react-icons/fa6';
+import { FaBrain, FaBriefcase, FaChartBar, FaRegEye } from 'react-icons/fa';
+import { FaGear, FaHand, FaLaptopCode, FaPenRuler } from 'react-icons/fa6';
 import { IoMdHand } from 'react-icons/io';
 import { MdOutlineWbSunny } from 'react-icons/md';
+import { IoColorPalette } from 'react-icons/io5';
+import { HiMagnifyingGlass } from 'react-icons/hi2';
 
 export const HomePage = () => {
-  const [showBlockModal, setShowBlockModal] = useState(false);
-  const showModalRef = useRef<HTMLButtonElement>(null);
-  // const [showDialogModal, setShowDialogModal] = useState(false);
-  const modalRef = useRef<HTMLDialogElement>(null);
   const { width } = useWindowSize();
-
-  const toggleBlockModal = () => {
-    setShowBlockModal((state) => !state);
-    if (showBlockModal) {
-      showModalRef.current?.focus();
-    }
-  };
 
   return (
     <>
-      {/* <Emoji name="check-mark-button" /> */}
       <div className="home-page-wrapper">
-        <div><h1> Веб-доступность:</h1>
-        <h2>цифровая инклюзивность для всех</h2></div>
-        
+        <div>
+          <h1> Веб-доступность:</h1>
+          <h2>цифровая инклюзивность для всех</h2>
+        </div>
+
         <p className={width > 720 ? 'subheading' : ''}>
           Веб-доступность — это практика создания сайтов и приложений, которыми
           могут пользоваться люди с любыми возможностями. Она обеспечивает
@@ -40,10 +29,6 @@ export const HomePage = () => {
           цифровые продукты доступными, следуя стандартам WCAG.
         </p>
         <h3>Основные принципы веб-доступности</h3>
-        {/* <button ref={showModalRef} onClick={(e) => toggleBlockModal(e)}>Show BlockModal</button>
-        <button onClick={() => modalRef.current?.showModal()}>
-          Show DialogModal
-        </button> */}
         <div className="cards-wrapper">
           <PrincipleCard
             header="Воспринимаемость"
@@ -108,12 +93,13 @@ export const HomePage = () => {
         </div>
 
         <h3>Структура сайта</h3>
-        <section>
+        <section className={width > 720 ? 'subheading' : ''}>
           <p>
             Сайт разделён на пять основных разделов: введение (эта страница),
             четыре принципа WCAG и практические примеры доступных модальных
             окон:
           </p>
+          <br />
           <ul>
             <li>
               <TextLink href="/perceivable">Воспринимаемость</TextLink>
@@ -131,10 +117,67 @@ export const HomePage = () => {
               <TextLink href="/modal">Модальные окна</TextLink>
             </li>
           </ul>
+          <br />
           <p>
             Вы можете использовать клавиатуру для навигации, увеличивать
             увеличивать масштаб или пользоваться сайтом на мобильных
             устройствах.
+          </p>
+        </section>
+
+        <h3>Доступность - это ответственность</h3>
+        <section>
+          <p style={{ fontSize: '1.125rem' }}>
+            <strong>Следование стандартам — лишь минимальная планка. </strong>
+            Настоящая профессиональная зрелость разработчика проявляется в
+            <b> осознанной социальной ответственности</b>, выходящей за рамки
+            формальных требований.
+          </p>
+
+          <br />
+          <h4>Доступность должна быть интегрирована на всех этапах: </h4>
+          <br />
+          <br />
+          <div className="cards-wrapper">
+            <PrincipleCard
+              icon="bar-chart"
+              iconElement={<FaPenRuler size={50} color="#3767CB" />}
+              header="Проектирование"
+              content={
+                'Инклюзивность закладывается на старте через сценарии, учитывающие потребности пользователей с любыми возможностями.'
+              }
+            />
+            <PrincipleCard
+              icon="sun"
+              header="Дизайн"
+              iconElement={<IoColorPalette size={50} color="#EE8EAC" />}
+              content={
+                'Визуальные решения объединяют эстетику и функциональность, гарантируя ясность даже при масштабировании или изменении режимов восприятия.'
+              }
+            />
+            <PrincipleCard
+              icon="briefcase"
+              iconElement={<FaLaptopCode size={50} color="#E67E58" />}
+              header="Разработка"
+              content={
+                'Семантический код и встроенные стандарты HTML/ARIA становятся основой для взаимодействия со вспомогательными технологиями.'
+              }
+            />
+            <PrincipleCard
+              icon="briefcase"
+              iconElement={<HiMagnifyingGlass size={50} color="#3767CB" />}
+              header="Тестирование"
+              content={
+                'Соответствие стандартам проверяется инструментами, но истинную доступность подтверждают пользователи с ОВЗ.'
+              }
+            />
+          </div>
+          <br />
+          <br />
+          <p style={{ textAlign: 'center', fontSize: '1.25rem' }}>
+            Ценности инклюзивности должны стать не внешними ограничениями, а{' '}
+            <b>внутренней нормой</b>. Только тогда технологии обретут подлинную
+            универсальность.
           </p>
         </section>
 
@@ -153,9 +196,6 @@ export const HomePage = () => {
           </ButtonLink>
         </section>
       </div>
-
-      {/* <BlockModal show={showBlockModal} onAction={toggleBlockModal} />
-      <DialogModal ref={modalRef} /> */}
     </>
   );
 };
